cf8bb601f5920f99025882456b041f8f44da1cb8
ViktorUngur002 <viktorungur02@gmail.com>
2024-10-26 15:38:11 +0200

    destructor examples

--- Tema1/src/main.cpp
+++ Tema1/src/main.cpp
@@ -11,18 +11,20 @@ namespace tema1
         int age;
         int salary = 0;
         int taxes = 300;
+        double* height;
 
         public:
         // multiple constructors to initialize different attributes
         Person()
         {
-
+            height = (double *) malloc(sizeof(double));
         }
 
         Person(std::string n, int a)
         {
             name = n;
             age = a;
+            height = (double *) malloc(sizeof(double));
         }
 
         Person(std::string n, int a, int s) // overloading
@@ -30,6 +32,24 @@ namespace tema1
             name = n;
             age = a;
             salary = calculateSalary(s);
+            height = (double *) malloc(sizeof(double));
+        }
+
+        Person(std::string n, int a, int s, double h)
+        {
+            name = n;
+            age = a;
+            salary = calculateSalary(s);
+            height = (double *) malloc(sizeof(double)); //allocating space on the heap
+            *height = h;
+        }
+
+        ~Person()
+        {
+            //stack is cleaned automatically, heap is cleaned manually
+            std::cout << "Destructor running, destroying: " << name << std::endl;
+            free(height); //the space that was manually allocated need to be freed, no memory leaks
+            // also file handles or network connections can be closed in the destructor
         }
 
         void setAge(int newAge)
@@ -53,6 +73,11 @@ namespace tema1
 
 using namespace tema1;
 
+void testDestructor()
+{
+    Person p("John", 23);
+} // destructor called here, after the program terminates
+
 int main()
 {
     Person p0;
@@ -65,7 +90,12 @@ int main()
     //p1.age = 10; // gives error
     p1.setAge(20); // the correct way to do it
 
+    testDestructor();
+
     p1.display();
     p2.display();
+
+    Person* p3 = new Person("Johnny", 45);
+    delete p3; //the destructor is called explicitly
     return 0;
 }
